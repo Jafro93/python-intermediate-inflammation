441615bdb8254fcc7afebf6c564b5051c41dea9b
Jack Foster <jack.foster@ukaea.uk>
2022-02-01 13:30:27 +0000

    Added precondition to new function to check data type and shape

--- inflammation/models.py
+++ inflammation/models.py
@@ -52,10 +52,17 @@ def patient_normalise(data):
     NaN values are ignored, and normalised to 0.
 
     Negative values are rounded to 0.
+
+    :param data: 2d array of inflammation data
+    :type data: ndarray
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensionalin')
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
-        
+
     maxima = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / maxima[:, np.newaxis]
